3352e38aadf6aaba9530c597463afa754a1a464e
anashamimi <anashamimi2@gmail.com>
2023-05-24 17:33:37 +0200

    add table in db with migration file

--- database/migrations/2023_05_24_144313_create_trains_table.php
+++ database/migrations/2023_05_24_144313_create_trains_table.php
@@ -15,6 +15,16 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('agency', 50);
+            $table->string('departure_station', 50);
+            $table->string('arrival_station', 50);
+            $table->time('departure_time');
+            $table->time('arrival_time');
+            $table->date('date_travel');
+            $table->string('train_code', 10);
+            $table->tinyInteger('number_carriages');
+            $table->boolean('in_time')->nullable()->default(false);
+            $table->boolean('deleted')->nullable()->default(false);
             $table->timestamps();
         });
     }
